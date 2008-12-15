0085e617b5789d57dc2b8572f6f2483f8b1dda43
Andrei Shishlo <shishlo@hotmail.com>
2008-12-15 19:37:02 +0000

    tracker3dfield renamed to laserstripping - intermediate state

---
+++ wrap_laserstripping.hh
@@ -0,0 +1,16 @@
+#ifndef WRAP_RK4_TRACKER_H
+#define WRAP_RK4_TRACKER_H
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	void inittrackerrk4(void);
+	
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif // WRAP_TRACKER_3D_FIELD_H
